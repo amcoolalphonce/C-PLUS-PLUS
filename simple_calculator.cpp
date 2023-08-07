80157710957b81a42bf6932ce11c44df60447cde
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-07 22:16:56 +0300

    Update simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 int main()
 {
-    
+    cout<<"*******A BASIC CALCULATOR*****" << endl;
+    cout<<"1. ADDITION"<<endl;
+    cout<<"2. SUBTRACTION"<<endl;
+    cout<<"3. DIVISION"<<endl;
+    cout<<"4. MULTIPLICATION"<<endl;
+    int choice;
+    cout<<"Enter a function to use: "<<endl;
+    cin>>choice;
+    if (choice == 1)
 }
