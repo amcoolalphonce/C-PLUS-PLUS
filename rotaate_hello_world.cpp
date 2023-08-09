697dd7d1707a3584e16c78b827de82dd7f8cb133
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-09 13:19:22 +0300

    Update rotaate_hello_world.cpp

--- rotaate_hello_world.cpp
+++ rotaate_hello_world.cpp
@@ -3,3 +3,17 @@
 #include<windows.h>
 
 using namespace std;
+int main()
+{
+    char helloString[5] ={'H','E','L','L','O'};
+
+    cout<<"ROTATION 0 AND 360 DEGREES"<<endl;
+    cout<<endl<<endl<<endl<<endl<<endl;
+    for (int i=0; i<=4;i++)
+    {
+        cout<< helloString[i]<<" ";
+    }
+    cout<<endl;
+    Sleep(1000);
+    
+}
