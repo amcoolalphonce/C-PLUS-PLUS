988752828fb9f7ed881258e7bbe3cc80b264f5d2
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-09 13:20:21 +0300

    Update rotaate_hello_world.cpp

--- rotaate_hello_world.cpp
+++ rotaate_hello_world.cpp
@@ -15,5 +15,17 @@ int main()
     }
     cout<<endl;
     Sleep(1000);
-    
+
+    cout<<"ROTATION 45 DEGREES"<<endl;
+    cout<<endl<<endl<<endl<<endl<<endl;
+    for(int i=0;i<5;i++)
+    {
+        for(int spaces=0; spaces<i;spaces++)
+        {
+            cout<<" ";
+        }
+    cout<< helloString[i]<<endl;
+    }
+    cout<<endl;
+    Sleep(1000);
 }
