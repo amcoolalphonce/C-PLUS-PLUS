b8af25a9afad54df3e0c24571b853fd3649fb299
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-10 22:21:10 +0300

    Subtraction

--- calculator.cpp
+++ calculator.cpp
@@ -45,4 +45,12 @@ int main()
         cin>>Number1>>Number2;
         cout<<Number1<<"+"<< Number2<<"="<<(Number1+Number2)<<endl;
     }
+
+    else if(selectedFunction==2)//SUBTRACTION SELECTED
+    {
+        cout<<"ENTER TWO NUMBERS TO SUBTRACT:"<<endl;
+        int Number1,Number2;
+        cin>>Number1>>Number2;
+        cout<<Number1<<"-"<<Number2<<"=="<<(Number1-Number2)<<endl;
+    }
 }
