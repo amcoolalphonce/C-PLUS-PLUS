c334f98c8fb2da9486aab6629d9265baea8f73e5
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-10 22:18:58 +0300

    Update calculator.cpp
    List  of functions

--- calculator.cpp
+++ calculator.cpp
@@ -2,3 +2,39 @@
 #include <cmath>
 
 using namespace std;
+
+int main()
+{
+    cout << "CASIO CALCULATOR" << endl;
+    cout<<"LIST OF FUNCTIONS:"<<endl;
+    cout<<"[1]SUMMATION"<<endl;
+    cout<<"[2]SUBTRACTION"<<endl;
+    cout<<"[3]MULTIPLICATION"<<endl;
+    cout<<"[4]DIVISION"<<endl;
+    cout<<"[5]MODULUS"<<endl;
+    cout<<"[6]TRUNCUATE"<<endl;
+    cout<<"[7]CEIL"<<endl;
+    cout<<"[8]POWER"<<endl;
+    cout<<"[9]COSINE"<<endl;
+    cout<<"[10]SINE"<<endl;
+    cout<<"[11]TANGENT"<<endl;
+    cout<<"[12]LOGARITHM"<<endl;
+    cout<<"[13]SQUAREROOT"<<endl;
+    cout<<"[14]HYPERBOLIC TANGENT"<<endl;
+    cout<<"[15]HYPERBOLIC COSINE"<<endl;
+    cout<<"[16]HYPERBOLIC SINE"<<endl;
+    cout<<"[17]NATURAL LOGARITHM"<<endl;
+    cout<<"[18]SQUARE"<<endl;
+    cout<<"[19]ROUNDING OFF"<<endl;
+    cout<<"[20]INVERSE OF TANGENT"<<endl;
+    cout<<"[21]INVERSE OF COSINE"<<endl;
+    cout<<"[22]INVERSE OF SINE"<<endl;
+    cout<<"[23]FLOOR"<<endl;
+    cout<<"[24]CUBE"<<endl;
+
+    cout<<endl;
+    cout<<endl;
+    cout<< "SELECT A FUNCTION TO USE:";
+    int selectedFunction;
+    cin >> selectedFunction;
+}
