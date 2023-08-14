3e33b4716ef8e98bb7c6ff623ac55a0db5d060d1
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-14 17:58:05 +0300

    Update sum_of_matrices.cpp

--- sum_of_matrices.cpp
+++ sum_of_matrices.cpp
@@ -17,4 +17,12 @@ int main()
 
     sum[2][2]={{2,2},{2,2}};
     sum[rows][columns]={}
+
+    for(i=0;i<rows;i++)for(j=0;j<columns;j++)
+    {
+        cin>>one[i][j];
+    }
+    //note that since we are adding the dimension of matrix ONE==TWO
+    cout<<"Enter matrix two members"<<endl;
+    for(i=0;i<rows;i++) for(j=0;j<columns;j++)
 }
