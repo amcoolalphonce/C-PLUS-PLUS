76a929fdd37e2cc48cee0bc4a8997c57360ae32f
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-14 17:33:36 +0300

    Update sum_of_matrices.cpp

--- sum_of_matrices.cpp
+++ sum_of_matrices.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
-    nt rows, columns, j, i;
+    int rows, columns, j, i;
     int one[columns][rows], two[columns][rows];
+
+    cout<<"Enter the number of rows of the matrix \n";
+    cin>>rows;
+    cout<<"\nEnter the number of columns of the matrix \n";
+    cin>>columns;
 }
