b11c2301171122475dc7fbf38da1ada543c88d45
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-15 15:28:44 +0300

    Update prime_numbers.cpp

--- prime_numbers.cpp
+++ prime_numbers.cpp
@@ -5,8 +5,11 @@ usinng namsespce std;
 int main()
 {
     int i,j;
-    for (int i=2; i<100; i++)
+    for (int i=2; i<100; i++)//rime numbers between 2 and 100
     {
-        
+        for(j=2;j<=i/j;j++)
+        {
+            if (!(i%j)):
+        }
     }
 }
