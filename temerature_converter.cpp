ba30859f5af3d74cbda47732d0b6fb28fcc8f0fb
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-20 20:34:06 +0300

    Fahrenheit to degrees

--- temerature_converter.cpp
+++ temerature_converter.cpp
@@ -3,6 +3,17 @@ using namespace std;
 int main()
 {
         char unit;
+        double temeperature
         cout<<"Enter the temperature unit you want to convert: C for Celsius or F for Fahrenheit:  "<<endl;
-        cin>>char;
+        cin>>unitr;
+
+        if (unit == 'F' || unit == 'f') 
+        {
+                cout << "Enter the temperature in Fahrenheit: ";
+                cin >> temperature;
+
+                // Convert Fahrenheit to Celsius
+                double celsius = (temperature - 32) * 5 / 9;
+        }
+
 }
