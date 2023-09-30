58e658759a85c9f52bdd24c39478e2e435e389c0
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-01 00:06:31 +0300

    Setup function

--- snake_game.cpp
+++ snake_game.cpp
@@ -10,3 +10,14 @@ int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+
+void Setup()
+{
+        gameOver = false;
+        dir = STOP;
+        x = width / 2;
+        y = height / 2;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        score = 0;
+}
