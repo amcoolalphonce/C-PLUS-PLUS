62889e14f95ed11344fb0106b53b31c13b9ba95e
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-01 00:04:57 +0300

    Initial state variables

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+using namespace std;
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
