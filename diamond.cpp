d91e30ceaaa2473cadcede50b186213aa0c97eb8
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-29 22:00:21 +0300

    Update diamond.cpp

--- diamond.cpp
+++ diamond.cpp
@@ -20,11 +20,12 @@ int main()
                 for(i = rows-1; i>= 1; i--)
                 {
                         cout<<" ";
-                        for(j= 1; j<=2*i-1; j++)
-                        {
-                                cout<<"*";
-                                cout<<endl;
-                        }
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                        cout << "*";
+                        cout << endl;
                 }
         }
+        return 0;
 }
