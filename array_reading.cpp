b28857e2703d23713a5fd759646a807ef846862f
Amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-01-20 20:30:41 +0300

    Update array_reading.cpp

--- array_reading.cpp
+++ array_reading.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
 
+using namespace std;
+
+int main(){
+  cout<<"READ YOUR ARRAY"<<endl;
+  int myArray[5]={21,24,25,26,28};
+  int k=0;
+  //initialization steps
+  //{PRECONDITION} I
+    while (k<5){
+      //{I^A}
+    cout<<myArray[k]<<endl;
+    k++;
+    //{I^!B}-->{Q}
+    //TRUE SO THE CODE TERMINATES
+  }
+}
